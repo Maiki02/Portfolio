420740a99067566efd7e4c55b93ea525aca97ab9
Maiki02 <miqueasdavidgentile@gmail.com>
2025-04-10 16:39:20 -0300

    Arreglamos estilos y agregamos footer

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { MenuComponent } from '../menu/menu.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
     selector: 'app-layout',
     imports: [
         HeaderComponent,
-        MenuComponent
+        MenuComponent,
+        FooterComponent
     ],
     templateUrl: './layout.component.html',
     styleUrl: './layout.component.scss'
