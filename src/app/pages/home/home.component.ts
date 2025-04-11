7773097627be9fcf7545ac70b68012b7e8ca36b3
m.gentile <m.gentile@bimtrazer.com>
2025-04-11 14:01:32 -0300

    Fondo de burbujas animado

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { LayoutComponent } from '../../shared/components/layout/layout.component';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-home',
-    imports: [LayoutComponent],
+    imports: [
+        CommonModule,
+        HeaderComponent
+    ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
---
+++ src/app/pages/library/library.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { LayoutComponent } from '../../shared/components/layout/layout.component';
+
+@Component({
+  selector: 'app-library',
+  imports: [
+    LayoutComponent
+  ],
+  templateUrl: './library.component.html',
+  styleUrl: './library.component.scss'
+})
+export class LibraryComponent {
+
+}
