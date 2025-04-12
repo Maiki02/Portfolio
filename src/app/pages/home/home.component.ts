a4c869ea3a30f925d4c412030101ff2784211a42
Maiki02 <miqueasdavidgentile@gmail.com>
2025-04-12 15:24:23 -0300

    Animaciones de botones

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,11 +21,52 @@ import { StartButtonComponent } from '@/shared/components/start-button/start-but
 })
 export class HomeComponent {
     
+    private INDIVIDUAL_ANIMATION_DURATION = 800;
+    private isOutHeader = false;
+    private isOutName = false;
+    private isOutButton = false;
+
+
     public theme$: Observable<"light" | "dark">
     constructor(private store:Store<AppState>, private themeSvc:ThemeService){
         this.theme$ = this.themeSvc.theme$
     }
 
+    startAnimation(){
+        this.setIsOutHeader(true);
+
+        setTimeout(() => {
+            this.setIsOutName(true);
+        }, this.INDIVIDUAL_ANIMATION_DURATION - 100);
+
+        setTimeout(() => {
+            this.setIsOutButton(true);
+        }, this.INDIVIDUAL_ANIMATION_DURATION * 2 - 300);
+    }
+
+    public getIsOutHeader() {
+        return this.isOutHeader;
+    }
+
+    public setIsOutHeader(isOutHeader: boolean) {
+        this.isOutHeader = isOutHeader;
+    }
+
+    public getIsOutName() {
+        return this.isOutName;
+    }
+
+    public setIsOutName(isOutName: boolean) {
+        this.isOutName = isOutName;
+    }
+
+    public getIsOutButton() {
+        return this.isOutButton;
+    }
+
+    public setIsOutButton(isOutButton: boolean) {
+        this.isOutButton = isOutButton;
+    }
 
 
     
--- src/app/shared/components/start-button/start-button.component.ts
+++ src/app/shared/components/start-button/start-button.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-start-button',
@@ -10,14 +10,40 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrl: './start-button.component.scss'
 })
 export class StartButtonComponent {
+  @Output() clicked = new EventEmitter<void>();
 
   x = 0;
   y = 0;
   show = false;
+
+  private ANIMATION_DURATION =2000;
+  private isExpanded = false;
   
   onMouseMove(event: MouseEvent) {
     this.x = event.offsetX;
     this.y = event.offsetY;
+  }
+
+  goToLibrary() {
+    console.log("TODO vamos a la librería")
+  }
+
+  getIsExpanded() {
+    return this.isExpanded;
+  }
+
+  setIsExpanded(isExpanded: boolean) {
+    this.isExpanded = isExpanded;
+  }
+
+  startAnimation(){
+    this.setIsExpanded(true);
+    this.clicked.emit();
+
+    setTimeout(() => {
+      this.setIsExpanded(false);
+      this.goToLibrary();
+    }, this.ANIMATION_DURATION);
   }
   
 }
