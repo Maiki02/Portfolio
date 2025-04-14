17a38027845854468983c252ac1d7848689d49f0
m.gentile <m.gentile@bimtrazer.com>
2025-04-14 16:02:59 -0300

    Ajustes visuales, footer, modo oscuro, layout

--- src/app/shared/components/menu/burguer-icon/burguer-icon.component.ts
+++ src/app/shared/components/menu/burguer-icon/burguer-icon.component.ts
@@ -1,21 +1,25 @@
+import { ThemeService } from '@/shared/services/theme.service';
 import { setIsMenuOpen } from '@/store/options/options.actions';
 import { AppState } from '@/store/types';
+import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-burguer-icon',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './burguer-icon.component.html',
   styleUrl: './burguer-icon.component.scss'
 })
 export class BurguerIconComponent implements OnInit, OnDestroy {
   private isOpen:boolean = false;
-
+  public theme$: Observable<"light" | "dark">
   private isOpen$!:Subscription;
-  constructor(private store:Store<AppState>){
-
+  constructor(private store:Store<AppState>, private themeSvc:ThemeService) {
+    this.theme$ = this.themeSvc.theme$
   }
 
   ngOnInit(): void {
