042e532d5f681714b79bf5df09a06f98dbe30bc8
Maiki02 <miqueasdavidgentile@gmail.com>
2025-04-17 17:10:36 -0300

    Button chat ia

---
+++ src/app/models/chat-ia/components/button-chat/button-chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-chat',
+  imports: [],
+  templateUrl: './button-chat.component.html',
+  styleUrl: './button-chat.component.scss'
+})
+export class ButtonChatComponent {
+
+}
