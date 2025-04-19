b434398abe71517dfd0b589378358d7ab46348b0
m.gentile <m.gentile@bimtrazer.com>
2025-04-19 18:26:28 -0300

    Corrección del header y menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ThemeButtonsComponent } from '../theme-buttons/theme-buttons.component';
 import { LanguageButtonsComponent } from '../language-buttons/language-buttons.component';
 import { BurguerIconComponent } from '../menu/burguer-icon/burguer-icon.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-header',
     imports: [
         ThemeButtonsComponent,
-        LanguageButtonsComponent
+        LanguageButtonsComponent,
+        CommonModule
     ],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+    @Input() isInHome: boolean = false
 }
