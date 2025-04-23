aeccb9ce945809eb09fa20691018a3b91fa7b43b
m.gentile <m.gentile@bimtrazer.com>
2025-04-23 19:47:37 -0300

    Contenido de education

--- src/app/models/education/components/card/card.component.ts
+++ src/app/models/education/components/card/card.component.ts
@@ -1,8 +1,13 @@
+import { ThemeService } from '@/shared/services/theme.service';
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-card-education',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
@@ -10,4 +15,11 @@ export class CardEducationComponent {
   @Input({required: true}) title: string = '';
   @Input({required: true}) description: string = '';
   @Input({required: true}) date: string = '';
+  @Input({required: true}) imageName: string = '';
+
+  public theme$: Observable<"light" | "dark">
+
+  constructor(private themeSvc:ThemeService){
+    this.theme$ = this.themeSvc.theme$
+  }
 }
--- src/app/pages/education/education.component.ts
+++ src/app/pages/education/education.component.ts
@@ -22,4 +22,26 @@ export class EducationComponent implements OnInit {
   ngOnInit(): void {
     this.store.dispatch(setRoute({route: ROUTES.EDUCATION}));
   } 
+
+  getDescriptionUTN(){
+    return `• Formación integral orientada a la industria de los videojuegos.\n
+    • Foco en diseño interactivo, programación y arte digital.\n
+    • Trabajo en equipos multidisciplinarios.\n
+    • Participación en todas las etapas del desarrollo: creación, producción y comercialización.`
+  }
+
+  getDescriptionUNR(){
+    return `• Formación en fundamentos teóricos de la informática.\n
+    • Enfoque en matemáticas, algoritmos y estructuras de datos.\n
+    • Introducción a lenguajes formales, lógica y arquitectura de computadoras.\n
+    • Desarrollo de habilidades analíticas y resolución de problemas complejos.`
+  }
+
+  getDescriptionArgProg(){
+  return `• Formación en fundamentos de programación.\n
+  • Introducción a algoritmos, estructuras de datos y lógica computacional.\n
+  • Enfoque en lenguajes como JavaScript, HTML y CSS.\n
+  • Introducción a formación en datos e inteligencia artificial.`
+  
+  }
 }
